ec1f07fcb159c182f43a01e4ad987fbbed591d60
Mark Daniel Aguinaldo <>
2024-10-29 19:55:09 +0800

    Added Users

--- Table_Users.sql
+++ Table_Users.sql
@@ -1,13 +1,10 @@
 USE [API]
 GO
-
-/****** Object:  Table [dbo].[Users]    Script Date: 10/29/2024 7:48:21 PM ******/
+/****** Object:  Table [dbo].[Users]    Script Date: 10/29/2024 7:53:05 PM ******/
 SET ANSI_NULLS ON
 GO
-
 SET QUOTED_IDENTIFIER ON
 GO
-
 CREATE TABLE [dbo].[Users](
 	[ID] [int] IDENTITY(1,1) NOT NULL,
 	[Username] [nvarchar](50) NULL,
@@ -21,12 +18,14 @@ CREATE TABLE [dbo].[Users](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
+SET IDENTITY_INSERT [dbo].[Users] ON 
 
+INSERT [dbo].[Users] ([ID], [Username], [Password], [Company], [IsAdmin], [Salt]) VALUES (8, N'test', N'a56d0f2473914a19d9147c6cba3c2b773631dc467cb466844636737a937ea19a', NULL, 0, N'4dEDIgycxAiNZGqFkrwU4Q==')
+INSERT [dbo].[Users] ([ID], [Username], [Password], [Company], [IsAdmin], [Salt]) VALUES (10, N'admin', N'acdc669b1ecda3826cf9ea920a33abefa57d31cdc26a73ad4de8d86ce06176df', 1, 1, N'ZmO5mjSnt9U2di/lxBSw4g==')
+SET IDENTITY_INSERT [dbo].[Users] OFF
+GO
 ALTER TABLE [dbo].[Users]  WITH CHECK ADD  CONSTRAINT [FK_Users_Company] FOREIGN KEY([Company])
 REFERENCES [dbo].[Company] ([ID])
 GO
-
 ALTER TABLE [dbo].[Users] CHECK CONSTRAINT [FK_Users_Company]
 GO
-
-
