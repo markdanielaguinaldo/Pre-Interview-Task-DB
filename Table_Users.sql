d0cfde3ef0893f59ece12a986388dbbdc3e7def6
Mark Daniel Aguinaldo <>
2024-10-29 18:54:10 +0800

    DB Solutions

---
+++ Table_Users.sql
@@ -0,0 +1,31 @@
+USE [API]
+GO
+
+/****** Object:  Table [dbo].[Users]    Script Date: 10/29/2024 6:49:25 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Users](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[Username] [nvarchar](50) NULL,
+	[Password] [nvarchar](50) NULL,
+	[Company] [int] NULL,
+	[IsAdmin] [bit] NOT NULL,
+ CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[Users]  WITH CHECK ADD  CONSTRAINT [FK_Users_Company] FOREIGN KEY([Company])
+REFERENCES [dbo].[Company] ([ID])
+GO
+
+ALTER TABLE [dbo].[Users] CHECK CONSTRAINT [FK_Users_Company]
+GO
+
+
